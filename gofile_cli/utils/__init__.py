5c781bbbf70c5035b162e11d6ef23109d9a0b446
2533584225@qq.com <2533584225@qq.com>
2025-06-27 09:05:00 +0800

    utils update

--- gofile_cli/utils/__init__.py
+++ gofile_cli/utils/__init__.py
@@ -1,8 +1,9 @@
 from .misc import random_string, validate_response
-from .utils import calculate_md5
+from .utils import calculate_md5, message_filter
 
 __all__ = (
     "random_string",
     "validate_response",
     "calculate_md5",
+    "message_filter",
 )
