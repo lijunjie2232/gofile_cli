3c39503e5910709b71ff2989c7c695f74ec35556
lijunjie2232 <2533584225@qq.com>
2026-03-30 19:27:45 +0900

    update

--- gofile_cli/__init__.py
+++ gofile_cli/__init__.py
@@ -1,37 +1,14 @@
 __version__ = "0.0.1"
 
 
-from gofile_cli.api import (
-    MailTM,
-    GoFile,
-)
-from gofile_cli.entity import (
-    Account,
-    Token,
-    Domains,
-    Domain,
-    Attachment,
-    Mapping,
-    Message,
-    MessageFromTo,
-    MessageSearch,
-    MessageSource,
-    MessageView,
-    Messages,
-    OneMessage,
-    MailTMInvalidResponse,
-    IpInfo,
-    Profile,
-    ProfileData,
-    StatsCurrent,
-)
-
+from gofile_cli.api import GoFile, MailTM
+from gofile_cli.entity import (Account, Attachment, Domain, Domains, IpInfo,
+                               MailTMInvalidResponse, Mapping, Message,
+                               MessageFromTo, Messages, MessageSearch,
+                               MessageSource, MessageView, OneMessage, Profile,
+                               ProfileData, StatsCurrent, Token)
 # from gofile_cli.cli import *
-from gofile_cli.utils import (
-    random_string,
-    validate_response,
-    calculate_md5,
-)
+from gofile_cli.utils import calculate_md5, random_string, validate_response
 
 __all__ = (
     "__version__",
---
+++ gofile_cli/__main__.py
@@ -0,0 +1,10 @@
+"""
+GoFile CLI - Command Line Interface for GoFile.io
+
+A modern, feature-rich command-line tool for interacting with GoFile.io file hosting service.
+"""
+
+from gofile_cli.cli import app
+
+if __name__ == "__main__":
+    app()
